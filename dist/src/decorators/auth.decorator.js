f12e96cf61235608c1a07a88cf60a5bf3abdacb2
Brandon-Paillacho-CMC <brandonpaillacho.clearminds@gmail.com>
2025-06-17 18:13:43 -0500

    fix: changed ignore files

---
+++ dist/src/decorators/auth.decorator.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Auth = Auth;
+const common_1 = require("@nestjs/common");
+const jwt_auth_guard_1 = require("../guards/jwt-auth.guard");
+function Auth(options = {}) {
+    const { isPublic = false, roles = [] } = options;
+    return (0, common_1.applyDecorators)((0, common_1.SetMetadata)('isPublic', isPublic), (0, common_1.SetMetadata)('roles', roles), (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard));
+}
+//# sourceMappingURL=auth.decorator.js.map
